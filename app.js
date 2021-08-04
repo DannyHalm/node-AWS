b07429bdb21092f8d89cf1a7a45151e3e9e6c6aa
Daniel Halmstrand <Daniel_Halmstrand@hotmail.com>
2021-08-04 08:56:05 +0100

    fixing PORT issue

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.get('/', (req,res)=>{
     res.send("Welcome to the home page");
 });
 
-app.listen(3000,()=>{
-    console.log("Server started and listening on port 3000");
+const port = process.env.port || 3000
+
+app.listen(port,()=>{
+    console.log(`Server started and listening on port ${port}`);
 });
